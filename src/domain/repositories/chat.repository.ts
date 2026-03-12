7a5354759cdea00245cee0fc0127db3b8de82bbc
quocs <quocsaodo@gmail.com>
2026-03-13 01:06:35 +0700

    feat: add ChatRepository interface with necessary entity imports

---
+++ src/domain/repositories/chat.repository.ts
@@ -0,0 +1,10 @@
+import { type ConversationEntity } from "../entities/conversation.entity.js";
+import { type ConversationUserEntity } from "../entities/conversation-user.entity.js";
+import { type MessageEntity } from "../entities/message.entity.js";
+import { type MessageMediaEntity } from "../entities/message-media.entity.js";
+import {type FollowEntity }from "../entities/follow.entity.js"
+
+
+export interface ChatRepository {
+
+}
